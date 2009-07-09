002856441fb0e9eb31cd0940a6260a80f9ae3115
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-09 15:54:29 -0500

    line the URLs up in the logical way

--- piano_man/urls.py
+++ piano_man/urls.py
@@ -4,14 +4,28 @@ from django.contrib import admin
 
 admin.autodiscover()
 
+ticket_urls = patterns('tickets.views',
+    url(r'^$', 'ticket_list', name='ticket_list'),
+    url(r'^new/$', 'new_ticket', name='new_ticket'),
+    url(r'^(?P<ticket_id>\d+)/$', 'ticket_detail', name='ticket_detail'),
+)
+
+
+repo_urls = patterns('',
+    url(r'^timeline/$', 'timeline.views.timeline', name='timeline'),
+    url(r'^tickets/', include(ticket_urls)),
+    url(r'commit/(?P<commit_id>.*)/$', 'django_vcs.views.commit_detail', name='commit_detail'),
+    url(r'browser/(?P<path>.*)$', 'django_vcs.views.code_browser', name='code_browser'),
+)
+
+
 urlpatterns = patterns('',
-    ('^admin/', include(admin.site.urls)),
-    ('^tickets/', include('tickets.urls')),
-    ('^repo/', include('django_vcs.urls')),
-    ('^timeline/', include('timeline.urls')),
+    url(r'^$', 'django_vcs.views.repo_list', name='repo_list'),
+    url(r'^(?P<slug>[\w-]+)/', include(repo_urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-        ('^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     )
