c618a73cc393e3c3fd9e8c6399eb775edbdbd56c
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-08 22:37:17 -0500

    added static support

--- piano_man/urls.py
+++ piano_man/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
 
@@ -9,3 +10,8 @@ urlpatterns = patterns('',
     ('^repo/', include('django_vcs.urls')),
     ('^timeline/', include('timeline.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        ('^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
