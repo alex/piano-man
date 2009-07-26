ae71d9149c1f232194821a0534d2197073f013dd
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-26 14:49:54 -0400

    provide links to upper directories from the code browsing views

--- piano_man/backstage/templatetags/backstage.py
+++ piano_man/backstage/templatetags/backstage.py
@@ -1,4 +1,8 @@
+import os
+
 from django import template
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
 from django_vcs.models import CodeRepository
 
@@ -25,3 +29,17 @@ def get_other_repos(parser, token):
     repo = parser.compile_filter(bits.pop(0))
     varname = bits.pop()
     return OtherRepos(repo, varname)
+
+@register.filter
+def urlize_path(path, repo):
+    bits = path.split(os.path.sep)
+    parts = []
+    for i, bit in enumerate(bits[:-1]):
+        parts.append('<a href="%(url)s">%(path)s</a>' % {
+            'url': reverse('code_browser', kwargs={
+                'path': '/'.join(bits[:i+1])+'/',
+                'slug': repo.slug
+            }),
+            'path': bit,
+        })
+    return ' / '.join(parts + bits[-1:])
