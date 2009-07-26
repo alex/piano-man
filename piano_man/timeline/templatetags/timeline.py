95cac0210ed1deb98f15bbace64c965a463b11ee
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-26 14:22:32 -0400

    refactor the timeline ui

--- piano_man/timeline/templatetags/timeline.py
+++ piano_man/timeline/templatetags/timeline.py
@@ -4,8 +4,8 @@ from django.template.loader import render_to_string
 register = template.Library()
 
 @register.simple_tag
-def timeline_item(item, repo):
+def timeline_item(item, repo, media_url):
     return render_to_string(
         'timeline/%s_obj.html' % type(item).__name__.lower(),
-        {'item': item, 'repo': repo}
+        {'item': item, 'repo': repo, 'MEDIA_URL': media_url}
     )
